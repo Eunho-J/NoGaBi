b6e71d0c06ba215b54fe7323b1cbccfdc439b367
Eunho-J <jho6394@gmail.com>
2021-02-10 16:08:11 +0900

    esm adopted to gabilog-backend

--- React Project/gabilog-backend/src/index.js
+++ React Project/gabilog-backend/src/index.js
@@ -1,26 +1,5 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
+// 이 파일에서만 no-global-assign ESLint 옵션을 비활성화
+/* eslint-disable no-global-assign */
 
-const api = require('./api');
-
-const app = new Koa();
-const router = new Router();
-
-//라우터 설정 router settings
-
-router.use('/api', api.routes()); // api 라우트 적용
-
-router.get('/', (ctx) => {
-  ctx.body = 'home';
-});
-
-// 라우터 적용 전에 bodyParser 적용
-app.use(bodyParser());
-
-// app 라우터 적용
-app.use(router.routes()).use(router.allowedMethods());
-
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
-});
+require = require('esm')(module /*, options*/);
+module.exports = require('./main.js');
